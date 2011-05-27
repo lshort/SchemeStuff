10049f3d2782072da197d6e20807cf3c257233a6
Lee Short <blackcat@pro-ns.net>
2011-05-27 13:24:10 -0700

    New files

---
+++ SimpleInheritance.scm
@@ -0,0 +1,73 @@
+#lang scheme
+
+(define (make-account balance)
+  (define (set-balance x) (set! balance x))
+  (define (get-balance) balance)
+  (make-account-internal set-balance get-balance))
+
+(define (make-account-internal setbal getbal)
+  (define (withdraw amount)
+     (if (>= (getbal) amount)
+         (begin (setbal (- (getbal) amount))
+                (getbal))
+         "Insufficient Funds"))
+  (define (deposit amount)
+     (setbal (+ (getbal) amount))
+     (getbal))
+  (define (copy setb getb)
+     (make-account-internal setb getb))
+  (define (dispatch msg)
+     (cond ((eq? msg 'withdraw) withdraw)
+           ((eq? msg 'deposit) deposit)
+           ((eq? msg 'copy) copy)
+           (else (error "Unknown request"
+                        msg))))
+  dispatch)
+
+
+(define (make-overdraft-account balance)
+  (let ((set-balance (lambda (x) (set! balance x)))
+        (get-balance (lambda () balance)))
+    (make-overdraft-account-internal set-balance get-balance)))
+
+(define (make-overdraft-account-internal setbal getbal)
+  (let  ((parent-dispatch (make-account-internal setbal 
+                                                 getbal)))
+    (define (withdraw amount)
+      (if (and (>= (getbal) 0) 
+               (>= (+ (getbal) 100) amount))
+          (begin (setbal (- (getbal) amount))
+                 (getbal))
+          "Insufficient Funds"))
+    (define (dispatch msg)
+      (cond ((eq? msg 'withdraw) withdraw)
+            (else (parent-dispatch msg))))
+    dispatch))
+
+
+(define (make-limited-account balance)
+  (let ((set-balance (lambda (x) (set! balance x)))
+        (get-balance (lambda () balance)))
+     (make-limited-account-internal set-balance get-balance)))
+
+(define (make-limited-account-internal setbal getbal) 
+  (let  ((parent-dispatch '())
+          (limit 1000))
+    (define (deposit amount)
+      (if (<= (+ (getbal) amount) limit)
+          (begin (setbal (+ (getbal) amount))
+                 (getbal))
+          "Over Limit"))
+    (define (set-proto proto)
+      (set! parent-dispatch ((proto 'copy) setbal getbal)))
+    (define (dispatch msg)
+      (cond ((eq? msg 'deposit) deposit)
+            ((eq? msg 'set-proto) set-proto)
+            ((not (null? parent-dispatch)) (parent-dispatch msg))
+            (else (((error "Unknown request" msg))))
+    dispatch))
+
+
+(define acc (make-account 50))
+(define acc3 (make-limited-account 50))
+(define acc2 (make-overdraft-account 50))
